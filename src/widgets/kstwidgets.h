e79956ee1b209a9a79d44ff0853f742a1d86b92f
Adam Treat <treat@kde.org>
2007-02-28 01:28:52 +0000

    * Make this a little easier.  Rest of plugins to follow
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@637876 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- src/widgets/kstwidgets.h
+++ src/widgets/kstwidgets.h
@@ -38,37 +38,31 @@
 
 #include <QtPlugin>
 
-#include "curveappearancewidget.h"
-
-class CurveAppearanceWidgetPlugin : public QObject, public QDesignerCustomWidgetInterface {
-  Q_OBJECT
-  Q_INTERFACES(QDesignerCustomWidgetInterface)
-
+class KstWidgetPlugin : public QObject, public QDesignerCustomWidgetInterface {
 public:
-  CurveAppearanceWidgetPlugin(QObject *parent = 0) : QObject(parent), _initialized(false) {}
+  KstWidgetPlugin(QObject *parent = 0) : QObject(parent), _initialized(false) {}
 
-  bool isContainer() const {
-    return false;
-  }
-  bool isInitialized() const {
-    return _initialized;
-  };
-  QIcon icon() const { return QIcon(); }
-
-  QString domXml() const {
-       return "<widget class=\"CurveAppearanceWidget\" name=\"curveAppearanceWidget\">\n"
-              "</widget>\n";
-  }
-
-  QString group() const { return "Kst Widgets"; }
-  QString includeFile() const { return "curveappearancewidget.ui.h"; }
-  QString name() const { return "CurveAppearanceWidget"; }
+  QString group() const { return tr("Kst Widgets"); }
   QString toolTip() const { return ""; }
   QString whatsThis() const { return ""; }
 
-  QWidget *createWidget(QWidget *parent) {
-    return new CurveAppearanceWidget(parent);
+  QString instanceName() const {
+    QChar camel = name().at(0).toLower();
+    return name().replace(0,1,camel.toLower());
   }
+
+  QString includeFile() const {
+    return name().toLower() + ".h";
+  }
+
+  QString domXml() const {
+    return QString::fromUtf8("<widget class=\"%1\" name=\"%2\"/>")
+            .arg(name()).arg(instanceName().toLower());
+  }
+
+  bool isContainer() const { return false; }
+  bool isInitialized() const { return _initialized; }
+  QIcon icon() const { return QIcon(); }
 
   void initialize(QDesignerFormEditorInterface *) {
     if (_initialized)
@@ -80,7 +74,28 @@ public:
 private:
   bool _initialized;
 };
+
+#include "curveappearancewidget.h"
+class CurveAppearanceWidgetPlugin : public KstWidgetPlugin {
+  Q_OBJECT
+  Q_INTERFACES(QDesignerCustomWidgetInterface)
+public:
+  CurveAppearanceWidgetPlugin(QObject *parent = 0) : KstWidgetPlugin(parent) {}
+  QString name() const { return "CurveAppearanceWidget"; } //do not translate
+  QWidget *createWidget(QWidget *parent) { return new CurveAppearanceWidget(parent); }
+};
 Q_EXPORT_PLUGIN2(curveappearancewidgetplugin, CurveAppearanceWidgetPlugin)
+
+// #include "curveplacementwidget.h"
+// class CurvePlacementWidgetPlugin : public KstWidgetPlugin {
+//   Q_OBJECT
+//   Q_INTERFACES(QDesignerCustomWidgetInterface)
+// public:
+//   CurvePlacementWidgetPlugin(QObject *parent = 0) : KstWidgetPlugin(parent) {}
+//   QString name() const { return "CurvePlacementWidget"; } //do not translate
+//   QWidget *createWidget(QWidget *parent) { return new CurvePlacementWidget(parent); }
+// };
+// Q_EXPORT_PLUGIN2(curveplacementwidgetplugin, CurvePlacementWidgetPlugin)
 
 #endif
 // vim: ts=2 sw=2 et
