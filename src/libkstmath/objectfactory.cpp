be90d3c6a59be4ee5598d610b852e984f979a7d7
George Staikos <staikos@kde.org>
2007-06-01 00:35:50 +0000

    add an object factory that can be used to make objects independent of the document.  However it only works for data objects.  I don't want to go too far with this until the .kst file structure is finalized
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@670303 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ src/libkstmath/objectfactory.cpp
@@ -0,0 +1,61 @@
+/***************************************************************************
+ *                                                                         *
+ *   copyright : (C) 2007 The University of Toronto                        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "objectfactory.h"
+
+#include <QMap>
+
+namespace Kst {
+
+static QMap<QString, ObjectFactory*> *factories = 0;
+
+void cleanupObjects() {
+  delete factories;
+  factories = 0;
+}
+
+
+ObjectFactory::ObjectFactory() {
+}
+
+
+void ObjectFactory::registerFactory(const QString& node, ObjectFactory *factory) {
+  if (!factories) {
+    factories = new QMap<QString,ObjectFactory*>;
+  }
+  factories->insert(node, factory);
+}
+
+
+void ObjectFactory::registerFactory(const QStringList& nodes, ObjectFactory *factory) {
+  foreach (const QString n, nodes) {
+    registerFactory(n, factory);
+  }
+}
+
+
+KstDataObjectPtr ObjectFactory::parse(QXmlStreamReader& stream) {
+  if (!factories) {
+    return 0;
+  }
+
+  ObjectFactory *f = factories->value(stream.name().toString());
+  if (!f) {
+    return 0;
+  }
+
+  return f->generateObject(stream);
+}
+
+
+}
+
+// vim: ts=2 sw=2 et
---
+++ src/libkstmath/objectfactory.h
@@ -0,0 +1,38 @@
+/***************************************************************************
+ *                                                                         *
+ *   copyright : (C) 2007 The University of Toronto                        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef OBJECTFACTORY_H
+#define OBJECTFACTORY_H
+
+#include <QXmlStreamReader>
+#include <QStringList>
+
+#include "kstdataobject.h"
+
+namespace Kst {
+
+class ObjectFactory {
+  public:
+    ObjectFactory();
+    virtual ~ObjectFactory() = 0;
+
+    // This takes ownership
+    static void registerFactory(const QString& node, ObjectFactory *factory);
+    static void registerFactory(const QStringList& nodes, ObjectFactory *factory);
+    static KstDataObjectPtr parse(QXmlStreamReader& stream);
+    virtual KstDataObjectPtr generateObject(QXmlStreamReader& stream) = 0;
+};
+
+}
+
+#endif
+
+// vim: ts=2 sw=2 et
