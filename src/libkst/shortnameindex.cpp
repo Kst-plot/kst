d03f4c634a7873fe1d6cbaf55bc538efad6a0166
Barth Netterfield <netterfield@astro.utoronto.ca>
2008-05-09 13:30:29 +0000

    Add forgotten file for short names.
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@805865 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ src/libkst/shortnameindex.cpp
@@ -0,0 +1,12 @@
+namespace Kst {
+  int _vnum = 1; // vectors
+  int _pnum = 1; // plugins
+  int _csdnum = 1; // csd
+  int _cnum = 1; // curves
+  int _enum = 1; // equations
+  int _hnum = 1; // histograms
+  int _inum = 1; // images
+  int _psdnum = 1; // psd
+  int _xnum = 1; // scalars
+  int _tnum = 1; // text string
+}
