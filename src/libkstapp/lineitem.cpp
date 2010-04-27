13d6230694f193c8010b85f1961bf3d73f12fc0c
Peter Kümmel <syntheticpp@gmx.net>
2010-04-27 19:47:39 +0000

    remove msvc warning
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@1119753 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- src/libkstapp/lineitem.cpp
+++ src/libkstapp/lineitem.cpp
@@ -9,9 +9,6 @@
  *                                                                         *
  ***************************************************************************/
 
-#ifdef _MSC_VER
-#define _USE_MATH_DEFINES
-#endif
 
 #include "lineitem.h"
 
