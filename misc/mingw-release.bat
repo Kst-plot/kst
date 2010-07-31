1cb1a20d313902efbf9631012197d5ef0f2f3ed3
Peter Kümmel <syntheticpp@gmx.net>
2010-07-31 13:10:29 +0000

    MinGW release: build all from scratch
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@1157540 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- misc/mingw-release.bat
+++ misc/mingw-release.bat
@@ -3,27 +3,27 @@
 :: TODO copy cmake files to sources
 
 set KSTDEPS=q:\kst-win32-deps-mingw
-rmdir /s/q %KSTDEPS
+rmdir /s/q %KSTDEPS%
 
 del CMakeCache.txt
-cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%KSTDEPS% Q:\netcdf\netcdf-4.1.1\netcdf-4.1.1\netcdf-windows
+cmake -G"MinGW Makefiles" -DCMAKE_VERBOSE_MAKEFILE=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%KSTDEPS% Q:\netcdf\netcdf-4.1.1\netcdf-4.1.1\netcdf-windows
 mingw32-make install
 
 
 del CMakeCache.txt
-cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%KSTDEPS% Q:\getdata\getdata\cmake
+cmake -G"MinGW Makefiles" -DCMAKE_VERBOSE_MAKEFILE=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%KSTDEPS% Q:\getdata\getdata\cmake
 mingw32-make install
 
 
 del CMakeCache.txt
-cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%KSTDEPS% Q:\gsl\gsl-1.14\gsl-1.14
+cmake -G"MinGW Makefiles" -DCMAKE_VERBOSE_MAKEFILE=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%KSTDEPS% Q:\gsl\gsl-1.14\gsl-1.14
 mingw32-make install
 
 
 
 set GETDATADIR=%KSTDEPS%
 set NETCDFDIR=%KSTDEPS%
-set GFLDIR=%KSTDEPS%
+set GSLDIR=%KSTDEPS%
 
 qmake ..\kst\kst.pro CONFIG+=release -r
 
