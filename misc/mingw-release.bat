ceb49d66de2cd0493a087bf9e61da84b57134486
Peter Kümmel <syntheticpp@gmx.net>
2011-01-27 18:59:24 +0000

    update mingw build script
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@1217521 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- misc/mingw-release.bat
+++ misc/mingw-release.bat
@@ -6,27 +6,35 @@ set KSTDEPS=q:\kst-win32-deps-mingw
 rmdir /s/q %KSTDEPS%
 
 del CMakeCache.txt
-cmake -G"MinGW Makefiles" -DCMAKE_VERBOSE_MAKEFILE=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%KSTDEPS% Q:\netcdf\netcdf-4.1.1\netcdf-4.1.1\netcdf-windows
+cmake ..\kst\misc\netcdf-windows -Dnetcdf_sources=Q:/netcdf/netcdf-4.1.1/netcdf-4.1.1 -G"MinGW Makefiles" -DCMAKE_VERBOSE_MAKEFILE=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%KSTDEPS%
 mingw32-make install
 
 
 del CMakeCache.txt
-cmake -G"MinGW Makefiles" -DCMAKE_VERBOSE_MAKEFILE=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%KSTDEPS% Q:\getdata\getdata\cmake
+cmake Q:\getdata\getdata\cmake -G"MinGW Makefiles" -DCMAKE_VERBOSE_MAKEFILE=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%KSTDEPS%
 mingw32-make install
 
 
 del CMakeCache.txt
-cmake -G"MinGW Makefiles" -DCMAKE_VERBOSE_MAKEFILE=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%KSTDEPS% Q:\gsl\gsl-1.14\gsl-1.14
+cmake ..\kst\misc\gsl-windows Q:\gsl\gsl-1.14\gsl-1.14 -G"MinGW Makefiles" -DCMAKE_VERBOSE_MAKEFILE=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%KSTDEPS%
+mingw32-make install
+
+del CMakeCache.txt
+cmake ..\kst\misc\cfitsio-windows -DCFIT_DIR=Q:/cfits/cfit3250 -G"MinGW Makefiles" -DCMAKE_VERBOSE_MAKEFILE=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%KSTDEPS% 
 mingw32-make install
 
 
+set GETDATA_DIR=%KSTDEPS%
+set NETCDF_DIR=%KSTDEPS%
+set GSL_DIR=%KSTDEPS%
+set CFITSIO_DIR=%KSTDEPS%
 
-set GETDATADIR=%KSTDEPS%
-set NETCDFDIR=%KSTDEPS%
-set GSLDIR=%KSTDEPS%
+del CMakeCache.txt
+rmdir /s/q bin
+rmdir /s/q lib
+rmdir /s/q plugin
 
-rmdir /s/q build
-
-qmake ..\kst\kst.pro CONFIG+=release -r
+cmake ..\kst\cmake -G"MinGW Makefiles" -Dkst_release=1 -Dkst_merge_files=1 
 
 mingw32-make
+mingw32-make install
