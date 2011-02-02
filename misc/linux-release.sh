e016afae73b129a12cf60ad501e4be5c5398f5a3
Peter Kümmel <syntheticpp@gmx.net>
2011-02-02 21:53:45 +0000

    add script to build on linux and upload to sf.net
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@1218564 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ misc/linux-release.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+kst_install_prefix=$1
+
+cmake ../kst/cmake -Dkst_release=1 -Dkst_merge_files=1 -Dkst_install_prefix=$kst_install_prefix
+
+make 
+make package
+make package_source
+
+if [ -z ${2} ]
+then
+	# spaces in $2 "Kst\\ 2.0.3"
+	scp ${kst_install_prefix}.sh             syntheticpp,kst@frs.sourceforge.net:"/home/frs/project/k/ks/kst/$2"
+	scp ${kst_install_prefix}-sources.tar.gz syntheticpp,kst@frs.sourceforge.net:"/home/frs/project/k/ks/kst/$2"
+	scp ${kst_install_prefix}-sources.zip    syntheticpp,kst@frs.sourceforge.net:"/home/frs/project/k/ks/kst/$2"
+fi
+
