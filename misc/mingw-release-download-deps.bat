cd829da2c5c78753c0dfbdc5bfd320711162153c
Peter Kümmel <syntheticpp@gmx.net>
2011-02-02 15:49:47 +0000

    release with cmake
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@1218497 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- misc/mingw-release-download-deps.bat
+++ misc/mingw-release-download-deps.bat
@@ -1,5 +1,6 @@
 
-cmake ..\kst\cmake -G"MinGW Makefiles" -Dkst_release=1 -Dkst_merge_files=1 -Dkst_3rdparty_download=1
+cmake ..\kst\cmake -G"MinGW Makefiles" -Dkst_release=1 -Dkst_merge_files=1 -Dkst_3rdparty_download=1 -Dkst_install_prefix=Kst-2.0.3-beta1
 
 mingw32-make
-mingw32-make install/strip
+mingw32-make package
+
