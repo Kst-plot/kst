86bd38fc7dc46d039624a4afd26f268646608d2f
Peter Kümmel <syntheticpp@gmx.net>
2011-02-10 15:57:55 +0000

    don't hardcode kst version
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@1219729 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- misc/mingw-release-download-deps.bat
+++ misc/mingw-release-download-deps.bat
@@ -1,5 +1,5 @@
 
-cmake ..\kst\cmake -G"MinGW Makefiles" -Dkst_release=1 -Dkst_merge_files=1 -Dkst_3rdparty_download=1 -Dkst_install_prefix=Kst-2.0.3-beta2
+cmake ..\kst\cmake -G"MinGW Makefiles" -Dkst_release=1 -Dkst_merge_files=1 -Dkst_3rdparty_download=1 -Dkst_install_prefix=%1
 
 mingw32-make
 mingw32-make package
