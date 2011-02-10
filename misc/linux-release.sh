22637b4ca5f27f636846232e4d9cc1e495b5c4ad
Peter Kümmel <syntheticpp@gmx.net>
2011-02-10 15:14:27 +0000

    cleanup linux release scripts
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@1219725 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- misc/linux-release.sh
+++ misc/linux-release.sh
@@ -1,20 +1,32 @@
 #!/bin/sh
 
+# Usage
+#   linux-release <package name> optional: <existing sourceforge dir>  <sourceforge username> <skip-build>
+# e.g:
+#   build  only     : linux-release.sh Kst-2.0.3-beta2 
+#   build and upload: linux-release.sh Kst-2.0.3-beta2 "Kst\\ 2.0.3" username
+#   upload only     : linux-release.sh Kst-2.0.3-beta2 "Kst\\ 2.0.3" username skip
+#   
 
 kst_install_prefix=$1
 
-cmake ../kst/cmake -Dkst_release=1 -Dkst_merge_files=1 -Dkst_install_prefix=$kst_install_prefix
-
-make -j4
-make package
-make package_source
-
-if [ ! -z ${2} ]
+if [ -z ${4} ]
 then
-	# spaces in $2 "Kst\\ 2.0.3"
-	scp ${kst_install_prefix}.sh             syntheticpp,kst@frs.sourceforge.net:"/home/frs/project/k/ks/kst/$2"
-	scp ${kst_install_prefix}.tar.gz         syntheticpp,kst@frs.sourceforge.net:"/home/frs/project/k/ks/kst/$2"
-	scp ${kst_install_prefix}-sources.tar.gz syntheticpp,kst@frs.sourceforge.net:"/home/frs/project/k/ks/kst/$2"
-	scp ${kst_install_prefix}-sources.zip    syntheticpp,kst@frs.sourceforge.net:"/home/frs/project/k/ks/kst/$2"
+	cmake ../kst/cmake -Dkst_release=1 -Dkst_merge_files=1 -Dkst_install_prefix=$kst_install_prefix
+	make -j4
+	make package
+	make package_source
 fi
 
+
+if [ ! -z ${3} ]
+then
+	# spaces in $2 "Kst\\ 2.0.3"
+	scp \
+	${kst_install_prefix}.sh \
+	${kst_install_prefix}.tar.gz \
+	${kst_install_prefix}-sources.tar.gz \
+	${kst_install_prefix}-sources.tar.bz2 \
+	${kst_install_prefix}-sources.zip \
+	${3},kst@frs.sourceforge.net:"/home/frs/project/k/ks/kst/${2}"
+fi
