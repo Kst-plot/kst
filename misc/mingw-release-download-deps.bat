533f1c79fff95532cb04348d47822f876795bcca
Peter Kümmel <syntheticpp@gmx.net>
2011-02-13 18:35:48 +0000

    makes testing simpler
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@1220309 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- misc/mingw-release-download-deps.bat
+++ misc/mingw-release-download-deps.bat
@@ -2,6 +2,7 @@
 cmake ..\kst\cmake -G"MinGW Makefiles" -Dkst_release=1 -Dkst_merge_files=1 -Dkst_3rdparty_download=1 -Dkst_install_prefix=%1
 
 mingw32-make
+mingw32-make install
 mingw32-make package
 mingw32-make package_source
 
