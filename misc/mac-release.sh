dff44547dc548a5840379df3751144dcff3170dc
Peter Kümmel <syntheticpp@gmx.net>
2011-03-23 12:26:39 +0000

    mac script
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@1225799 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ misc/mac-release.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# $1 = path to kst/cmake
+# $2 = package name
+cmake $1 -Dkst_merge_files=1 -Dkst_3rdparty=1 -Dkst_dataobjects=1 -Dkst_install_prefix=$2 -Dkst_release=1
+
+make -j4
+
+#make package # ugly dmg produced by cmake
+make install
+hdiutil create -ov  -srcfolder $2 -volname $2 -format UDRW $2.dmg
+
