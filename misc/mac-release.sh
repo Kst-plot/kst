40c278faaff934d4edbd27d8ca9056f04a632845
Peter Kümmel <syntheticpp@gmx.net>
2011-03-23 12:30:18 +0000

    mac script
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@1225800 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- misc/mac-release.sh
+++ misc/mac-release.sh
@@ -6,7 +6,5 @@ cmake $1 -Dkst_merge_files=1 -Dkst_3rdparty=1 -Dkst_dataobjects=1 -Dkst_install_
 
 make -j4
 
-#make package # ugly dmg produced by cmake
-make install
-hdiutil create -ov  -srcfolder $2 -volname $2 -format UDRW $2.dmg
-
+#TODO Why is /build/bin/kst2 added to the dmg?
+make package
