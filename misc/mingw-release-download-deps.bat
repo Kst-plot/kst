cb6762560bae7ce28bb4646dd4b85f0731b1509a
Peter Kümmel <syntheticpp@gmx.net>
2011-04-28 20:20:26 +0000

    cmake: add option for kst version
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@1229656 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- misc/mingw-release-download-deps.bat
+++ misc/mingw-release-download-deps.bat
@@ -1,5 +1,5 @@
 
-cmake ..\kst\cmake -G"MinGW Makefiles" -Dkst_release=1 -Dkst_merge_files=1 -Dkst_3rdparty_download=1 -Dkst_install_prefix=%1
+cmake ..\kst\cmake -G"MinGW Makefiles" -Dkst_release=1 -Dkst_merge_files=1 -Dkst_3rdparty_download=1 -Dkst_install_prefix=Kst-%1 -Dkst_version_string=%1
 
 mingw32-make
 mingw32-make install
