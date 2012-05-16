d1494fedb49a92b1cf4ba9a72955021fc3b738bf
Peter Kümmel <syntheticpp@gmx.net>
2012-05-16 21:29:43 +0000

    mingw 4.7 fixes
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@1295139 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- misc/mingw-release-build-deps.bat
+++ misc/mingw-release-build-deps.bat
@@ -6,7 +6,7 @@ cd         mingw-release-build-deps
 cmake ..\kst\cmake -G"CodeBlocks - MinGW Makefiles" -Dkst_release=1 -Dkst_merge_files=0 -Dkst_3rdparty_build=1 -Dkst_install_prefix=Kst-%1 -Dkst_version_string=%1
 
 mingw32-make -j
-mingw32-make install
+mingw32-make install/strip
 mingw32-make package
 mingw32-make package_source
 
--- misc/mingw-release-download-deps.bat
+++ misc/mingw-release-download-deps.bat
@@ -6,7 +6,7 @@ cd         mingw-release-download-deps
 cmake ..\kst\cmake -G"CodeBlocks - MinGW Makefiles" -Dkst_release=1 -Dkst_merge_files=1 -Dkst_3rdparty_download=1 -Dkst_install_prefix=Kst-%1 -Dkst_version_string=%1
 
 mingw32-make -j
-mingw32-make install
+mingw32-make install/strip
 mingw32-make package
 mingw32-make package_source
 
