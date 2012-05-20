d593bcb663bcbe0a5dbcdb6695948c3667adfd93
Peter Kümmel <syntheticpp@gmx.net>
2012-05-20 13:17:57 +0000

    Merge branch 'deploy'
    Conflicts:
    	kst/cmake/CMakeLists.txt
    
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@1295756 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- misc/linux-release.sh
+++ misc/linux-release.sh
@@ -7,16 +7,30 @@
 #   build and upload: linux-release.sh 2.0.3-beta2 "Kst\\ 2.0.3" username
 #   upload only     : linux-release.sh 2.0.3-beta2 "Kst\\ 2.0.3" username skip
 #   
+set -x
 
 kst_install_prefix=kst-$1
 
+machine_type=`uname -m`
+binarytar=${kst_install_prefix}-binary-${machine_type}.tar
+
+
 if [ -z ${4} ]
 then
-	cmake ../kst/cmake -Dkst_release=1 -Dkst_merge_files=1 -Dkst_version_string=$1 -Dkst_install_prefix=$kst_install_prefix
-	make -j4
-	make install
-	make package
-	make package_source
+	cmake ../kst/cmake -Dkst_release=1 -Dkst_merge_files=1 -Dkst_version_string=$1 -Dkst_install_prefix=$kst_install_prefix -Dkst_3rdparty_build=1 -Dkst_deploy=1
+	cd 3rdparty
+	make -j
+	cd ..
+	make -j
+	make 
+	make install/strip
+	strip ${kst_install_prefix}/lib
+	strip ${kst_install_prefix}/lib/kst2/plugins/*
+	
+    rm	${binarytar}.gz
+    tar cf ${binarytar} ${kst_install_prefix}
+    gzip -9 ${binarytar}
+    make package_source
 fi
 
 
@@ -24,8 +38,7 @@ if [ ! -z ${3} ]
 then
 	# spaces in $2 "Kst\\ 2.0.3"
 	scp \
-	${kst_install_prefix}-binary.sh \
+	${binarytar}.gz \
 	${kst_install_prefix}.tar.gz \
-	${kst_install_prefix}.tar.bz2 \
 	${3},kst@frs.sourceforge.net:"/home/frs/project/k/ks/kst/${2}"
 fi
