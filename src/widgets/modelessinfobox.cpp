c83472b174f63ea190df42a1f1179009cf515c3e
Barth Netterfield <netterfield@astro.utoronto.ca>
2012-07-02 16:31:07 +0000

    Add licence...
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@1303761 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- src/widgets/modelessinfobox.cpp
+++ src/widgets/modelessinfobox.cpp
@@ -1,3 +1,21 @@
+/***************************************************************************
+                          modelessinfobox.cpp: a dialog with a read-only
+                          texedit in it for displaing help modelessly.
+                             -------------------
+    begin                : June, 2012
+    copyright            : (C) 2012 by C. Barth Netterfield
+    email                : netterfield@astro.utoronto.ca
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "modelessinfobox.h"
 #include "ui_modelessinfobox.h"
 
