4ba9a7321296b13284c9e5be6a8c2f4954da8434
Peter Kümmel <syntheticpp@gmx.net>
2012-07-05 19:51:24 +0000

    update to getdata 0.8.0
    build system fixes
    
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@1304205 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- misc/mingw-release-download-deps.bat
+++ misc/mingw-release-download-deps.bat
@@ -3,12 +3,13 @@ rmdir /s/q mingw-release-download-deps
 mkdir      mingw-release-download-deps
 cd         mingw-release-download-deps
 
-cmake ..\kst\cmake -G"CodeBlocks - MinGW Makefiles" -Dkst_release=1 -Dkst_merge_files=1 -Dkst_3rdparty_download=1 -Dkst_install_prefix=Kst-%1 -Dkst_version_string=%1
+::cmake ..\kst\cmake -G"CodeBlocks - MinGW Makefiles" -Dkst_release=1 -Dkst_merge_files=1 -Dkst_3rdparty_download=1 -Dkst_install_prefix=Kst-%1 -Dkst_version_string=%1
+cmake ..\kst\cmake -GNinja -Dkst_release=1 -Dkst_merge_files=1 -Dkst_3rdparty_download=1 -Dkst_install_prefix=Kst-%1 -Dkst_version_string=%1
 
-mingw32-make -j
-mingw32-make install/strip
-mingw32-make package
-mingw32-make package_source
+ninja 
+ninja install/strip
+ninja package
+ninja package_source
 
 cd ..
 
