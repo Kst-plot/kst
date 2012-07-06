27006576c4784254929fe83bcf80e13aa24a0667
Peter Kümmel <syntheticpp@gmx.net>
2012-07-06 10:34:03 +0000

    mac fixes
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@1304337 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- misc/mac-release.sh
+++ misc/mac-release.sh
@@ -5,7 +5,12 @@
 
 kst_install_prefix=Kst-$2
 
-cmake $1 -Dkst_merge_files=1 -Dkst_3rdparty=1 -Dkst_dataobjects=1 -Dkst_version_string=$2 -Dkst_release=1 -Dkst_install_prefix=$kst_install_prefix
+cmake $1 -Dkst_merge_files=1 -Dkst_3rdparty_build=1 -Dkst_dataobjects=1 -Dkst_version_string=$2 -Dkst_release=1 -Dkst_install_prefix=$kst_install_prefix
+
+cd 3rdparty
+make -j4
+make
+cd ..
 
 make -j4
 
