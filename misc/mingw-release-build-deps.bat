14054612a9ea0b9a7e17a9d915bc2df24cac9316
Peter Kümmel <syntheticpp@gmx.net>
2012-07-22 12:16:47 +0000

    update windows deploying
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@1307223 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- misc/mingw-release-build-deps.bat
+++ misc/mingw-release-build-deps.bat
@@ -1,14 +1,28 @@
+echo off 
 
 rmdir /s/q mingw-release-build-deps
 mkdir      mingw-release-build-deps
 cd         mingw-release-build-deps
 
-cmake ..\kst\cmake -G"CodeBlocks - MinGW Makefiles" -Dkst_release=1 -Dkst_merge_files=1 -Dkst_3rdparty_build=1 -Dkst_install_prefix=Kst-%1 -Dkst_version_string=%1
+if "%1" == "" (
+	set ver=2.0.x
+) else (
+	set ver=%1
+)
 
-mingw32-make 
-mingw32-make install/strip
-mingw32-make package
-mingw32-make package_source
+if "%2" == "" (
+	set make=mingw32-make
+) else (
+	set make=%2
+)
+
+echo cmake ..\kst\cmake -G"CodeBlocks - MinGW Makefiles" -Dkst_release=1 -Dkst_merge_files=1 -Dkst_3rdparty_build=1 -Dkst_install_prefix=Kst-%ver% -Dkst_version_string=%ver% -Dkst_make_program=%make%
+
+cmake ..\kst\cmake -G"CodeBlocks - MinGW Makefiles" -Dkst_release=1 -Dkst_merge_files=1 -Dkst_3rdparty_build=1 -Dkst_install_prefix=Kst-%ver% -Dkst_version_string=%ver% -Dkst_make_program=%make%
+
+%make%
+%make% install/strip
+%make% package
+%make% package_source
 
 cd ..
-
