7ec1a719dcb865ba37df7e83a70ec688390d4c40
Peter Kümmel <syntheticpp@gmx.net>
2012-10-14 12:03:02 +0000

    add file with character traits
    git-svn-id: svn://anonsvn.kde.org/home/kde/branches/work/kst/portto4/kst@1320472 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ src/datasources/ascii/asciicharactertraits.h
@@ -0,0 +1,124 @@
+/***************************************************************************
+ *                                                                         *
+ *   Copyright : (C) 2010 The University of Toronto                        *
+ *   email     : netterfield@astro.utoronto.ca                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef ASCII_CHARACTER_TRAITS_H
+#define ASCII_CHARACTER_TRAITS_H
+
+#include <QString>
+
+namespace AsciiCharacterTraits
+{
+
+struct LineEndingType {
+  bool is_crlf;
+  char character;
+  bool isCR() const { return character == '\r'; }
+  bool isLF() const { return character == '\n'; }
+};
+
+// column and comment delimiter functions
+
+struct AlwaysTrue {
+  AlwaysTrue() {
+  }
+  inline bool operator()() const {
+    return true;
+  }
+};
+
+struct AlwaysFalse {
+  AlwaysFalse() {
+  }
+  inline bool operator()() const {
+    return false;
+  }
+};
+
+struct NoDelimiter {
+  NoDelimiter() {
+  }
+  inline bool operator()(const char) const {
+    return false;
+  }
+};
+
+struct  IsWhiteSpace {
+  IsWhiteSpace() {
+  }
+  inline bool operator()(const char c) const {
+    return c == ' ' || c == '\t';
+  }
+};
+
+struct IsDigit {
+  IsDigit() {
+  }
+  inline bool operator()(const char c) const {
+    return (c >= 48) && (c <= 57) ? true : false;
+  }
+};
+
+struct IsCharacter {
+  IsCharacter(char c) : character(c) {
+  }
+  const char character;
+  inline bool operator()(const char c) const {
+    return character == c;
+  }
+};
+
+struct IsInString {
+  IsInString(const QString& s) : str(s), chars(s.size()) {
+    QByteArray ascii = str.toLatin1();
+    for (int i = 0; i < 6 && i < chars; i++) {
+      ch[i] = ascii[i];
+    }
+  }
+  const QString str;
+  const int chars;
+  char ch[6];
+  inline bool operator()(const char c) const {
+    switch (chars) {
+    case 0: return false;
+    case 1: return ch[0]==c;
+    case 2: return ch[0]==c || ch[1]==c;
+    case 3: return ch[0]==c || ch[1]==c || ch[2]==c;
+    case 4: return ch[0]==c || ch[1]==c || ch[2]==c || ch[3]==c;
+    case 5: return ch[0]==c || ch[1]==c || ch[2]==c || ch[3]==c || ch[4]==c;
+    case 6: return ch[0]==c || ch[1]==c || ch[2]==c || ch[3]==c || ch[4]==c || ch[5]==c;
+    default: return str.contains(c);
+    }
+  }
+};
+
+struct IsLineBreakLF {
+  IsLineBreakLF(const LineEndingType&) : size(1) {
+  }
+  const int size;
+  inline bool operator()(const char c) const {
+    return c == '\n';
+  }
+};
+
+struct IsLineBreakCR {
+  IsLineBreakCR(const LineEndingType& t) : size( t.is_crlf ? 2 : 1 ) {
+  }
+  const int size;
+  inline bool operator()(const char c) const {
+    return c == '\r';
+  }
+};
+
+}
+
+#endif
+// vim: ts=2 sw=2 et
