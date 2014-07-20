0ac49d4a165ed50f12320fb1edb35d6156fccce8
Peter Kümmel <syntheticpp@gmx.net>
2014-07-20 11:33:01 +0200

    add script to get selected translations

--- cmake/travis.sh
+++ cmake/travis.sh
@@ -262,18 +262,7 @@ fi
 # checkout translations
 #
 cd $startdir
-./l10n-fetch-po-files.py
-
-# only ship listed tranlations
-mkdir po_all
-mv po/* po_all
-for tr in ca ca@valencia de en_GB fr nl pl pt pt_BR sv uk
-do
-    file=kst_common_$tr.po
-    echo "using translation $tr"
-    cp po_all/$file po
-done
-
+./get-translations
 cd $builddir
 
 
