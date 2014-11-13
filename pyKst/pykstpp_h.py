b07adec078e25298b91be7b7b10ce90d85ea8b65
Barth Netterfield <netterfield@astro.utoronto.ca>
2014-11-12 19:35:55 -0500

    Remove obsolete files.

--- pyKst/pykstpp_h.py
+++
@@ -1,9 +0,0 @@
-# the followign for arch linux
-#INCLUDES=["/usr/include/QtCore","/usr/include/QtNetwork","/usr/include/Qt"]
-
-# the following for kubuntu
-INCLUDES=["/usr/include/qt4/QtCore","/usr/include/qt4/QtNetwork","/usr/include/qt4"]
-
-LIBS=["QtCore","QtNetwork"]
-LIBDIRS=["/usr/lib/x86_64-linux-gnu/", "/usr/lib/"]
-
