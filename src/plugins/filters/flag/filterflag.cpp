b8212219a215a8781ee296d73dbaa8569cda49cc
Barth Netterfield <netterfield@astro.utoronto.ca>
2016-02-11 15:52:05 -0500

    A plugin which flaggs data.
    When flag is non-0, output is set to nan.

---
+++ src/plugins/filters/flag/filterflag.cpp
@@ -0,0 +1,287 @@
+/***************************************************************************
+ *                                                                         *
+ *   copyright : (C) 2007 The University of Toronto                        *
+ *                   netterfield@astro.utoronto.ca                         *
+ *   copyright : (C) 2005  University of British Columbia                  *
+ *                   dscott@phas.ubc.ca                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#include "filterflag.h"
+#include "objectstore.h"
+#include "ui_filterflagconfig.h"
+#include "math_kst.h"
+
+
+static const QString& VECTOR_IN = "Y Vector";
+static const QString& VECTOR_FLAG_IN = "Flag Vector";
+static const QString& VECTOR_OUT = "Y";
+
+class ConfigWidgetFilterFlagPlugin : public Kst::DataObjectConfigWidget, public Ui_FilterFlagConfig {
+  public:
+    ConfigWidgetFilterFlagPlugin(QSettings* cfg) : DataObjectConfigWidget(cfg), Ui_FilterFlagConfig() {
+      _store = 0;
+      setupUi(this);
+    }
+
+    ~ConfigWidgetFilterFlagPlugin() {}
+
+    void setObjectStore(Kst::ObjectStore* store) {
+      _store = store;
+      _vector->setObjectStore(store);
+      _flag->setObjectStore(store);
+    }
+
+    void setupSlots(QWidget* dialog) {
+      if (dialog) {
+        connect(_vector, SIGNAL(selectionChanged(QString)), dialog, SIGNAL(modified()));
+        connect(_flag, SIGNAL(selectionChanged(QString)), dialog, SIGNAL(modified()));
+      }
+    }
+
+    void setVectorX(Kst::VectorPtr vector) {
+      setSelectedVector(vector);
+    }
+
+    void setVectorY(Kst::VectorPtr vector) {
+      setSelectedVector(vector);
+    }
+
+    void setVectorsLocked(bool locked = true) {
+      _vector->setEnabled(!locked);
+    }
+
+    Kst::VectorPtr selectedVector() { return _vector->selectedVector(); };
+    void setSelectedVector(Kst::VectorPtr vector) { return _vector->setSelectedVector(vector); };
+
+    Kst::VectorPtr selectedFlag() { return _flag->selectedVector(); };
+    void setSelectedFlag(Kst::VectorPtr vector) { return _flag->setSelectedVector(vector); };
+
+    virtual void setupFromObject(Kst::Object* dataObject) {
+      if (FilterFlagSource* source = static_cast<FilterFlagSource*>(dataObject)) {
+        setSelectedVector(source->vector());
+        setSelectedFlag(source->flagVector());
+      }
+    }
+
+    virtual bool configurePropertiesFromXml(Kst::ObjectStore *store, QXmlStreamAttributes& attrs) {
+      Q_UNUSED(store);
+      Q_UNUSED(attrs);
+
+      bool validTag = true;
+
+//       QStringRef av;
+//       av = attrs.value("value");
+//       if (!av.isNull()) {
+//         _configValue = QVariant(av.toString()).toBool();
+//       }
+
+      return validTag;
+    }
+
+  public slots:
+    virtual void save() {
+      if (_cfg) {
+        _cfg->beginGroup("Filter Flag Plugin");
+        _cfg->setValue("Input Vector", _vector->selectedVector()->Name());
+        _cfg->setValue("Flag Vector", _flag->selectedVector()->Name());
+        _cfg->endGroup();
+      }
+    }
+
+    virtual void load() {
+      if (_cfg && _store) {
+        _cfg->beginGroup("Filter Flag Plugin");
+        QString vectorName = _cfg->value("Input Vector").toString();
+        Kst::Object* object = _store->retrieveObject(vectorName);
+        Kst::Vector* vector = static_cast<Kst::Vector*>(object);
+        if (vector) {
+          setSelectedVector(vector);
+        }
+
+        vectorName = _cfg->value("Flag Vector").toString();
+        object = _store->retrieveObject(vectorName);
+        vector = static_cast<Kst::Vector*>(object);
+        if (vector) {
+          _flag->setSelectedVector(vector);
+        }
+        _cfg->endGroup();
+      }
+    }
+
+  private:
+    Kst::ObjectStore *_store;
+
+};
+
+
+FilterFlagSource::FilterFlagSource(Kst::ObjectStore *store)
+: Kst::BasicPlugin(store) {
+}
+
+
+FilterFlagSource::~FilterFlagSource() {
+}
+
+
+QString FilterFlagSource::_automaticDescriptiveName() const {
+  if (vector()) {
+    return tr("%1 Flagged", "arg 1 is the name of the vector which has been Flagged").arg(vector()->descriptiveName());
+  } else {
+    return tr("Flagged");
+  }
+}
+
+
+QString FilterFlagSource::descriptionTip() const {
+  QString tip;
+
+  tip = tr("Flag Filter: %1\n  Flag: %2").arg(Name()).arg(flagVector()->Name());
+
+  tip += tr("\nInput: %1").arg(vector()->descriptionTip());
+  return tip;
+}
+
+void FilterFlagSource::change(Kst::DataObjectConfigWidget *configWidget) {
+  if (ConfigWidgetFilterFlagPlugin* config = static_cast<ConfigWidgetFilterFlagPlugin*>(configWidget)) {
+    setInputVector(VECTOR_IN, config->selectedVector());
+    setInputVector(VECTOR_FLAG_IN, config->selectedFlag());
+  }
+}
+
+
+void FilterFlagSource::setupOutputs() {
+  setOutputVector(VECTOR_OUT, "");
+}
+
+
+bool FilterFlagSource::algorithm() {
+  Kst::VectorPtr inputVector = _inputVectors[VECTOR_IN];
+  Kst::VectorPtr flagVector = _inputVectors[VECTOR_FLAG_IN];
+  Kst::VectorPtr outputVector;
+  // maintain kst file compatibility if the output vector name is changed.
+  if (_outputVectors.contains(VECTOR_OUT)) {
+    outputVector = _outputVectors[VECTOR_OUT];
+  } else {
+    outputVector = _outputVectors.values().at(0);
+  }
+
+  int N = inputVector->length();
+  int i;
+  if (N < 1) {
+    return false;
+  }
+
+  // resize the output array
+  outputVector->resize(inputVector->length(), false);
+
+  for (i=0; i<N; ++i) {
+    if (flagVector->value(i)) {
+      outputVector->raw_V_ptr()[i] = Kst::NOPOINT;
+    } else {
+      outputVector->raw_V_ptr()[i] = inputVector->value(i);
+    }
+  }
+
+  Kst::LabelInfo label_info = inputVector->labelInfo();
+  label_info.name = tr("Flaged %1").arg(label_info.name);
+  outputVector->setLabelInfo(label_info);
+
+  return true;
+}
+
+
+Kst::VectorPtr FilterFlagSource::vector() const {
+  return _inputVectors[VECTOR_IN];
+}
+
+
+Kst::VectorPtr FilterFlagSource::flagVector() const {
+  return _inputVectors[VECTOR_FLAG_IN];
+}
+
+
+QStringList FilterFlagSource::inputVectorList() const {
+  QStringList input_vectors (VECTOR_IN);
+  input_vectors += VECTOR_FLAG_IN;
+
+  return input_vectors;
+}
+
+
+QStringList FilterFlagSource::inputScalarList() const {
+  return QStringList(  );
+}
+
+
+QStringList FilterFlagSource::inputStringList() const {
+  return QStringList( /*STRING_IN*/ );
+}
+
+
+QStringList FilterFlagSource::outputVectorList() const {
+  return QStringList( VECTOR_OUT );
+}
+
+
+QStringList FilterFlagSource::outputScalarList() const {
+  return QStringList( /*SCALAR_OUT*/ );
+}
+
+
+QStringList FilterFlagSource::outputStringList() const {
+  return QStringList( /*STRING_OUT*/ );
+}
+
+
+void FilterFlagSource::saveProperties(QXmlStreamWriter &s) {
+  Q_UNUSED(s);
+//   s.writeAttribute("value", _configValue);
+}
+
+
+// Name used to identify the plugin.  Used when loading the plugin.
+QString FilterFlagPlugin::pluginName() const { return tr("Flag Filter"); }
+QString FilterFlagPlugin::pluginDescription() const { return tr("Outputs the input vector flagged NaN when flag is non-zero."); }
+
+
+Kst::DataObject *FilterFlagPlugin::create(Kst::ObjectStore *store, Kst::DataObjectConfigWidget *configWidget, bool setupInputsOutputs) const {
+
+  if (ConfigWidgetFilterFlagPlugin* config = static_cast<ConfigWidgetFilterFlagPlugin*>(configWidget)) {
+
+    FilterFlagSource* object = store->createObject<FilterFlagSource>();
+
+    if (setupInputsOutputs) {
+      object->setupOutputs();
+      object->setInputVector(VECTOR_IN, config->selectedVector());
+      object->setInputVector(VECTOR_FLAG_IN, config->selectedFlag());
+    }
+
+    object->setPluginName(pluginName());
+
+    object->writeLock();
+    object->registerChange();
+    object->unlock();
+
+    return object;
+  }
+  return 0;
+}
+
+
+Kst::DataObjectConfigWidget *FilterFlagPlugin::configWidget(QSettings *settingsObject) const {
+  ConfigWidgetFilterFlagPlugin *widget = new ConfigWidgetFilterFlagPlugin(settingsObject);
+  return widget;
+}
+
+#ifndef QT5
+Q_EXPORT_PLUGIN2(kstplugin_FilterFlagPlugin, FilterFlagPlugin)
+#endif
+
+// vim: ts=2 sw=2 et
---
+++ src/plugins/filters/flag/filterflag.h
@@ -0,0 +1,78 @@
+/***************************************************************************
+ *                                                                         *
+ *   copyright : (C) 2016 C. Barth Netterfield                             *
+ *                   netterfield@astro.utoronto.ca                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef FILTERFLAG_H
+#define FILTERFLAG_H
+
+#include <QFile>
+
+#include <basicplugin.h>
+#include <dataobjectplugin.h>
+
+class FilterFlagSource : public Kst::BasicPlugin {
+  Q_OBJECT
+
+  public:
+    virtual QString _automaticDescriptiveName() const;
+
+    virtual QString descriptionTip() const;
+
+    Kst::VectorPtr vector() const;
+    Kst::VectorPtr flagVector() const;
+
+    virtual void change(Kst::DataObjectConfigWidget *configWidget);
+
+    void setupOutputs();
+    virtual bool algorithm();
+
+    virtual QStringList inputVectorList() const;
+    virtual QStringList inputScalarList() const;
+    virtual QStringList inputStringList() const;
+    virtual QStringList outputVectorList() const;
+    virtual QStringList outputScalarList() const;
+    virtual QStringList outputStringList() const;
+
+    virtual void saveProperties(QXmlStreamWriter &s);
+
+  protected:
+    FilterFlagSource(Kst::ObjectStore *store);
+    ~FilterFlagSource();
+
+  friend class Kst::ObjectStore;
+
+
+};
+
+
+class FilterFlagPlugin : public QObject, public Kst::DataObjectPluginInterface {
+    Q_OBJECT
+    Q_INTERFACES(Kst::DataObjectPluginInterface)
+    Q_PLUGIN_METADATA(IID "com.kst.DataObjectPluginInterface/2.0")
+  public:
+    virtual ~FilterFlagPlugin() {}
+
+    virtual QString pluginName() const;
+    virtual QString pluginDescription() const;
+
+    virtual DataObjectPluginInterface::PluginTypeID pluginType() const { return Filter; }
+
+    virtual bool hasConfigWidget() const { return true; }
+
+    virtual Kst::DataObject *create(Kst::ObjectStore *store, Kst::DataObjectConfigWidget *configWidget, bool setupInputsOutputs = true) const;
+
+    virtual Kst::DataObjectConfigWidget *configWidget(QSettings *settingsObject) const;
+};
+
+#endif // FILTERFLAG_H
+// vim: ts=2 sw=2 et
+
+
