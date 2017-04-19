348a560290a4d8bde39c0ed283df10047927cf07
Barth Netterfield <netterfield@astro.utoronto.ca>
2017-04-18 20:10:07 -0400

    Add license to new files.

--- src/libkstapp/datasourcedialog.h
+++ src/libkstapp/datasourcedialog.h
@@ -1,3 +1,15 @@
+/***************************************************************************
+ *                                                                         *
+ *   copyright : (C) 2017 C. Barth Netterfield
+ *                   netterfield@astro.utoronto.ca                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef DATASOURCEDIALOG_H
 #define DATASOURCEDIALOG_H
 
